245e2e3756a80dec2b9c872bdaab615e5736391a
Abhi998-arch <abhilash998575@gmail.com>
2026-08-22 16:15:49 +0530

    Clear repository

--- app/hr/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { AppShell } from "@/components/layout/AppShell";
-
-export default function HRLayout({ children }: { children: React.ReactNode }) {
-  return <AppShell role="hr">{children}</AppShell>;
-}
